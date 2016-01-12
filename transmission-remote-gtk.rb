3ea606eeca205b08c3b55d04a83cbccb0bb24a37
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2016-01-12 13:14:04 +0530

    transmission-remote-gtk: remove unnecessary x11 dependency

--- transmission-remote-gtk.rb
+++ transmission-remote-gtk.rb
@@ -3,7 +3,6 @@ class TransmissionRemoteGtk < Formula
   url "https://transmission-remote-gtk.googlecode.com/files/transmission-remote-gtk-1.1.1.tar.gz"
   sha256 "9f5ac2de623d633bcaacb6362bb6d8fb468c6dff278b867f65809c48ea1570c9"
 
-  depends_on :x11
   depends_on "intltool" => :build
   depends_on "pkg-config" => :build
   depends_on "gtk+3"
