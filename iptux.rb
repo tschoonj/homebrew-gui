fff53b2694414d578662d3176007697e8d5dc12e
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2016-01-12 10:18:55 +0530

    iptux: remove x11 dependency

--- iptux.rb
+++ iptux.rb
@@ -3,7 +3,6 @@ class Iptux < Formula
   url "https://github.com/iptux-src/iptux/archive/v0.6.2.tar.gz"
   sha256 "d567096d97b3b168dde1510f0ca00a13c6f8bb596131c234482377d7d8fb211b"
 
-  depends_on :x11
   depends_on "gettext"
   depends_on "gtk+"
   depends_on "gconf"
