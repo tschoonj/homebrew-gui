9a33a32478a57a1577f8f6a79f7c99be623f6287
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2016-01-18 09:38:40 +0530

    kernagic: fix build failure with older clang versions

--- kernagic.rb
+++ kernagic.rb
@@ -7,6 +7,9 @@ class Kernagic < Formula
   depends_on "pkg-config" => :build
   depends_on "gtk+"
 
+  # removes option not provided by older clangs
+  patch :DATA
+
   def install
     system "make"
     system "make", "install", "PREFIX=#{prefix}"
@@ -16,3 +19,17 @@ class Kernagic < Formula
     system "#{bin}/kernagic -h"
   end
 end
+__END__
+diff --git a/.mm/magic b/.mm/magic
+index 088e5d9..d9e0a47 100644
+--- a/.mm/magic
++++ b/.mm/magic
+@@ -6,7 +6,7 @@ CFLAGS += -Wall -Wextra
+ #CFLAGS += -g -O2 -rdynamic
+ CFLAGS += -fPIC
+ #CFLAGS += -fomit-frame-pointer
+-CFLAGS += -funroll-loops -fforce-addr -ftracer -fpeel-loops -fmerge-all-constants
++CFLAGS += -funroll-loops -fforce-addr -ftracer -fmerge-all-constants
+ CFLAGS += -Wno-unused-parameter  -Wno-sign-compare
+ CFLAGS +=  -I. -D_XOPEN_SOURCE=500
+ CFLAGS += -std=c99
