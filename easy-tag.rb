ed6790be51f24a680f31b08717a11bb7f85d4e31
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2016-01-26 22:02:39 +0530

    easy-tag 2.4.1

--- easy-tag.rb
+++ easy-tag.rb
@@ -1,8 +1,8 @@
 class EasyTag < Formula
   desc "Application for viewing and editing audio file tags"
   homepage "https://projects.gnome.org/easytag"
-  url "https://download.gnome.org//sources/easytag/2.4/easytag-2.4.0.tar.xz"
-  sha256 "b8d882bc168abca96b1b369b10e5b97fa29a7a43f7b33f34489768d111610c3d"
+  url "https://download.gnome.org//sources/easytag/2.4/easytag-2.4.1.tar.xz"
+  sha256 "b9ef5f47572f44985922230f94b5ac908d4c7a12430dac662fdf1b48c7e577d5"
 
   bottle do
     sha256 "1000f29472e58ce6e4b7c076b2ce4ff25cd67fcef7cda40fc7a46d15f74d8435" => :yosemite
@@ -27,10 +27,7 @@ class EasyTag < Formula
   depends_on "speex" => :optional
   depends_on "wavpack" => :optional
 
-  # fixes unknown linker flag error: bug was filed as https://bugzilla.gnome.org/show_bug.cgi?id=750981
-  # forces use of gtk3-update-icon-cache instead of gtk-update-icon-cache. No bugreport should
-  # be filed for this since it only occurs because Homebrew renames gtk+3's gtk-update-icon-cache
-  # to gtk3-update-icon-cache in order to avoid a collision between gtk+ and gtk+3.
+  # disable gtk-update-icon-cache
   patch :DATA
 
   def install
@@ -45,104 +42,26 @@ class EasyTag < Formula
 
   def post_install
     system "#{Formula["glib"].opt_bin}/glib-compile-schemas", "#{HOMEBREW_PREFIX}/share/glib-2.0/schemas"
+    system "#{Formula["gtk+3"].opt_bin}/gtk3-update-icon-cache", "-f", "-t", "#{HOMEBREW_PREFIX}/share/icons/hicolor"
   end
 
   test do
     system "#{bin}/easytag", "--version"
   end
 end
+
 __END__
 diff --git a/Makefile.in b/Makefile.in
-index 672f271..94e84a8 100644
+index 9dbde5f..4ffe52e 100644
 --- a/Makefile.in
 +++ b/Makefile.in
-@@ -1121,7 +1121,7 @@ dist_appiconscalable_DATA = \
- dist_appiconsymbolic_DATA = \
-	data/icons/symbolic/easytag-symbolic.svg
+@@ -3960,8 +3960,6 @@ data/org.gnome.EasyTAG.gschema.valid: data/.dstamp
+ @ENABLE_MAN_TRUE@		--path $(builddir)/doc --output $(builddir)/doc/ \
+ @ENABLE_MAN_TRUE@		http://docbook.sourceforge.net/release/xsl/current/manpages/docbook.xsl $<
+ 
+-install-data-hook: install-update-icon-cache
+-uninstall-hook: uninstall-update-icon-cache
+ 
+ install-update-icon-cache:
+ 	$(AM_V_at)$(POST_INSTALL)
 
--UPDATE_ICON_CACHE = gtk-update-icon-cache --ignore-theme-index --force
-+UPDATE_ICON_CACHE = gtk3-update-icon-cache --ignore-theme-index --force
- dist_noinst_DATA = \
-	$(appdata_in_files) \
-	$(desktop_in_files) \
-diff --git a/configure b/configure
-index 26a01f0..cfefab6 100755
---- a/configure
-+++ b/configure
-@@ -20466,77 +20466,6 @@ fi
-
-
-
--for flag in          -Wl,--no-as-needed              ; do
--  as_CACHEVAR=`$as_echo "ax_cv_check_cflags_$ax_compiler_flags_test_$flag" | $as_tr_sh`
--{ $as_echo "$as_me:${as_lineno-$LINENO}: checking whether C compiler accepts $flag" >&5
--$as_echo_n "checking whether C compiler accepts $flag... " >&6; }
--if eval \${$as_CACHEVAR+:} false; then :
--  $as_echo_n "(cached) " >&6
--else
--
--  ax_check_save_flags=$CFLAGS
--  CFLAGS="$CFLAGS $ax_compiler_flags_test $flag"
--  cat confdefs.h - <<_ACEOF >conftest.$ac_ext
--/* end confdefs.h.  */
--
--int
--main ()
--{
--
--  ;
--  return 0;
--}
--_ACEOF
--if ac_fn_c_try_compile "$LINENO"; then :
--  eval "$as_CACHEVAR=yes"
--else
--  eval "$as_CACHEVAR=no"
--fi
--rm -f core conftest.err conftest.$ac_objext conftest.$ac_ext
--  CFLAGS=$ax_check_save_flags
--fi
--eval ac_res=\$$as_CACHEVAR
--	       { $as_echo "$as_me:${as_lineno-$LINENO}: result: $ac_res" >&5
--$as_echo "$ac_res" >&6; }
--if eval test \"x\$"$as_CACHEVAR"\" = x"yes"; then :
--
--if ${WARN_LDFLAGS+:} false; then :
--
--  case $WARN_LDFLAGS in #(
--  "* $flag *|*$flag") :
--    { { $as_echo "$as_me:${as_lineno-$LINENO}: : WARN_LDFLAGS already contains \$flag"; } >&5
--  (: WARN_LDFLAGS already contains $flag) 2>&5
--  ac_status=$?
--  $as_echo "$as_me:${as_lineno-$LINENO}: \$? = $ac_status" >&5
--  test $ac_status = 0; } ;; #(
--  *) :
--
--     as_fn_append WARN_LDFLAGS " $flag"
--     { { $as_echo "$as_me:${as_lineno-$LINENO}: : WARN_LDFLAGS=\"\$WARN_LDFLAGS\""; } >&5
--  (: WARN_LDFLAGS="$WARN_LDFLAGS") 2>&5
--  ac_status=$?
--  $as_echo "$as_me:${as_lineno-$LINENO}: \$? = $ac_status" >&5
--  test $ac_status = 0; }
--     ;;
--esac
--
--else
--
--  WARN_LDFLAGS=$flag
--  { { $as_echo "$as_me:${as_lineno-$LINENO}: : WARN_LDFLAGS=\"\$WARN_LDFLAGS\""; } >&5
--  (: WARN_LDFLAGS="$WARN_LDFLAGS") 2>&5
--  ac_status=$?
--  $as_echo "$as_me:${as_lineno-$LINENO}: \$? = $ac_status" >&5
--  test $ac_status = 0; }
--
--fi
--
--else
--  :
--fi
--
--done
--
-
-     if test "$ax_enable_compile_warnings" != "no"; then :
