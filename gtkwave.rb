285afc537f8c2a93106a916decf67f5582a5daa3
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-06 03:37:37 +0100

    gtkwave: update 3.3.73 bottle.

--- gtkwave.rb
+++ gtkwave.rb
@@ -6,9 +6,9 @@ class Gtkwave < Formula
 
   bottle do
     cellar :any
-    sha256 "62fdd1795ed9e8e22f0cd491d338cee12083543fd65a3cf3cb0534f40cee1ce4" => :el_capitan
-    sha256 "a98ca9697eb5eab7993ad4d534e5f5a2a85e776916e3b592a9a4c11f7fdca457" => :yosemite
-    sha256 "60b0470f23ed6f7fb751a8f905b5c31f9c1f30a405271525a7046ca864ebb0dd" => :mavericks
+    sha256 "cf92e280c519b77a7f1f83262468230c0444fe60d9d1ccc7a14021ced2b07158" => :el_capitan
+    sha256 "ee9abb9b8de3d2f459baf453a6df4a9cac831736abbc95c021aee2f498a912a3" => :yosemite
+    sha256 "c43f10cff2379630a213a6bb61cd5eaf63d5ebf4d928ec25b4dbc47a91fbf2af" => :mavericks
   end
 
   depends_on "pkg-config" => :build
