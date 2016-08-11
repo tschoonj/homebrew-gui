5981d7d73b10ff43180a89df90c787f9efdf90f4
Miguel Araújo <miguelarauj1o@gmail.com>
2016-08-10 22:56:35 -0300

    geeqie: remove indefinite article
    Closes #23.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- geeqie.rb
+++ geeqie.rb
@@ -1,5 +1,5 @@
 class Geeqie < Formula
-  desc "A lightweight Gtk+ based image viewer"
+  desc "Lightweight Gtk+ based image viewer"
   homepage "http://geeqie.sourceforge.net/"
   url "https://downloads.sourceforge.net/project/geeqie/geeqie/geeqie-1.1/geeqie-1.1.tar.gz"
   sha256 "5544e81c29917a647f19bfe800d9f0dd1cd5b890329feebd9abd80927e1afecf"
