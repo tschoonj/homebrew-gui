601a7f39f88c1fa4c3e6674107ac481bd3d9b00a
Miguel Araújo <miguelarauj1o@gmail.com>
2016-08-10 22:57:56 -0300

    gsmartcontrol: remove indefinite article
    Closes #21.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- gsmartcontrol.rb
+++ gsmartcontrol.rb
@@ -1,5 +1,5 @@
 class Gsmartcontrol < Formula
-  desc "A graphical user interface for smartctl"
+  desc "Graphical user interface for smartctl"
   homepage "http://gsmartcontrol.sourceforge.net/home/index.php"
   url "https://downloads.sourceforge.net/project/gsmartcontrol/0.8.7/gsmartcontrol-0.8.7.tar.bz2"
   sha256 "708fa803243abb852ed52050fc82cd3592a798c02743342441996e77f19ffec6"
