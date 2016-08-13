69519efb751d4c4e88f84c260bc95d9463026eed
BrewTestBot <brew-test-bot@googlegroups.com>
2016-08-13 06:09:49 +0100

    transmission-remote-gtk: update 1.3.1 bottle.

--- transmission-remote-gtk.rb
+++ transmission-remote-gtk.rb
@@ -5,9 +5,9 @@ class TransmissionRemoteGtk < Formula
   sha256 "1b29c573b1e205e3e7c2433dc4a48f9574278d97e033845d19bbffa1d7f75345"
 
   bottle do
-    sha256 "31152453f9a1f37be6a8a9708f4fe7700c0c2beab0a7df17b0bc7697b57b8981" => :el_capitan
-    sha256 "71e03421172b0db9022536fd86b9df676773f1b34bd904107a689bcbba1791d2" => :yosemite
-    sha256 "fda18813fe8c12f406748e5f2b1976777cb8df579474b10ccbdfe80a6cca7acb" => :mavericks
+    sha256 "34fa40fca4ab814d305a086534ded7c969c4e2a3442ba5cfed4aa95cae27aee9" => :el_capitan
+    sha256 "e9fd55c23924db49a9189712e7b5b3aa53353f55da25638bbeb70244b8d0a5d5" => :yosemite
+    sha256 "0f9a9b18466c05566d54eb099ba719aa173240c243d4b5004e2d4a0f4180a898" => :mavericks
   end
 
   depends_on "intltool" => :build
