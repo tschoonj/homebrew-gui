1207cc6b6a8d209f5513a70dfb826bb586fdd6a3
Miguel Araújo <miguelarauj1o@gmail.com>
2016-09-03 11:38:21 -0300

    cardpeek: add desc and update code position (#18)

--- cardpeek.rb
+++ cardpeek.rb
@@ -1,8 +1,11 @@
 class Cardpeek < Formula
+  desc "Linux/Windows/OSX tool to read the contents of ISO7816 smart cards"
   homepage "http://pannetrat.com/Cardpeek/"
   url "http://downloads.pannetrat.com/install/cardpeek-0.8.4.tar.gz"
   mirror "https://github.com/DomT4/LibreMirror/raw/d1dff2e5714953308864a606618a85cbde2375a2/Cardpeek/cardpeek-0.8.4.tar.gz"
   sha256 "9c78dfdf84493c551b49447e4bb46c8d7b33f0785b93893222b70b6115013a85"
+
+  head "https://cardpeek.googlecode.com/svn/trunk/"
 
   bottle do
     cellar :any
@@ -10,8 +13,6 @@ class Cardpeek < Formula
     sha256 "7b2e3f1d05316f6f7d001898bfe6028ef98a9f40b561e4bd89bc1d77531d7dce" => :yosemite
     sha256 "16993be903b7a9c5e8cd64f930aa221837dcd387dc3af4e88c508cee585fed17" => :mavericks
   end
-
-  head "https://cardpeek.googlecode.com/svn/trunk/"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
